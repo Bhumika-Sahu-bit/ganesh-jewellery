e485da58126ae546ba1541b4b3d8178b39c632ba
bhumika sahu <bhumi16124@gmail.com>
2026-01-22 20:10:18 +0530

    backend-commit

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 //Socket.io setup
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: `process.env.FRONTEND_URL`,
     methods: ["GET", "POST"],
     credentials: true
   }
