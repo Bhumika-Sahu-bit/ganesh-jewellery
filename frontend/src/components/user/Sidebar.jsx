3a4ff07ae6ae5b0d6519100e75a8647afa709650
bhumika sahu <bhumi16124@gmail.com>
2026-04-08 23:52:37 +0700

    ui skeleton

--- frontend/src/components/user/Sidebar.jsx
+++ frontend/src/components/user/Sidebar.jsx
@@ -1,7 +1,6 @@
 
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import UserHomeSkeleton from "../skeletons/UserHomeSkeleton.jsx";
 
 import {
   AiFillHome,
